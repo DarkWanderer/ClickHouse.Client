28e8a6ed8f107e6494855344bf99abf9a461cf33
DarkWanderer <elquentaro@gmail.com>
2019-10-16 20:58:31 +0300

    More checks in "multiple types" test

--- ClickHouse.Client.Tests/SqlCommandsTestSuiteBase.cs
+++ ClickHouse.Client.Tests/SqlCommandsTestSuiteBase.cs
@@ -32,7 +32,7 @@ namespace ClickHouse.Client.Tests
             Assert.IsTrue(reader.HasRows);
             Assert.IsTrue(reader.Read());
 
-            CollectionAssert.AreEqual(new[]{ "a", "b", "c"}, Enumerable.Range(0, 3).Select(reader.GetName));
+            CollectionAssert.AreEqual(new[] { "a", "b", "c" }, Enumerable.Range(0, 3).Select(reader.GetName));
             CollectionAssert.AreEqual(new[] { 1, 2, 3 }, Enumerable.Range(0, 3).Select(reader.GetValue));
             Assert.IsFalse(reader.HasRows);
         }
@@ -44,7 +44,8 @@ namespace ClickHouse.Client.Tests
                 .Cast<DataType>()
                 .Select(dt => dt.ToString())
                 .Where(dt => dt.Contains("Int") || dt.Contains("Float"))
-                .Select(dt => $"to{dt.ToString()}(1)");
+                .Select(dt => $"to{dt.ToString()}(1)")
+                .ToArray();
             var sql = $"select {string.Join(',', types)}";
 
             using var connection = TestUtilities.GetTestClickHouseConnection(Driver);
@@ -53,13 +54,18 @@ namespace ClickHouse.Client.Tests
             using var reader = await command.ExecuteReaderAsync();
             Assert.IsTrue(reader.HasRows);
             Assert.IsTrue(reader.Read());
+            Assert.AreEqual(types.Length, reader.FieldCount);
+
+            for (int i = 0; i < reader.FieldCount; i++)
+                Assert.AreEqual("1", reader.GetValue(i).ToString());
+
+            Assert.IsFalse(reader.HasRows);
+            Assert.IsFalse(reader.Read());
         }
 
         [Test]
         public async Task ShouldSelectSingleColumnRange()
         {
-                //Assert.Inconclusive("ClickHouse returns incorrect type for 'SELECT number FROM system.numbers' in JSON");
-
             const int count = 100;
             using var connection = TestUtilities.GetTestClickHouseConnection(Driver);
             var command = connection.CreateCommand();
