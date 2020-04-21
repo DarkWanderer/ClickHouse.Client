302dc03c5445a7043108bb2c6ef2ed9df490301a
DarkWanderer <elquentaro@gmail.com>
2020-04-21 19:53:56 +0300

    Adding a test checking "ALTER TABLE DELETE" query #25

---
+++ ClickHouse.Client.Tests/SqlUpdateTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using System.Threading.Tasks;
+using ClickHouse.Client.ADO;
+using ClickHouse.Client.Utility;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests
+{
+    public class SqlUpdateTests
+    {
+        private readonly DbConnection connection;
+        public SqlUpdateTests()
+        {
+            var builder = TestUtilities.GetConnectionStringBuilder();
+            builder.UseSession = true;
+            builder.Driver = ClickHouseConnectionDriver.Binary;
+            builder.Compression = true;
+            connection = new ClickHouseConnection(builder.ToString());
+        }
+
+        [Test]
+        public async Task ShouldExecuteAlterTable()
+        {
+            await connection.ExecuteStatementAsync("CREATE TABLE IF NOT EXISTS test.table_delete_from (value Int32) ENGINE=MergeTree ORDER BY value");
+            await connection.ExecuteStatementAsync("ALTER TABLE test.table_delete_from DELETE WHERE 1=1");
+        }
+    }
+}
