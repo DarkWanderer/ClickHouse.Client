5da44c9db6eb8e7f9cd8a6fe3fb31ab1938fb4c0
DarkWanderer <elquentaro@gmail.com>
2020-06-23 16:54:21 +0300

    More functions for ClickHouseRawResult

--- ClickHouse.Client/ADO/Readers/ClickHouseRawResult.cs
+++ ClickHouse.Client/ADO/Readers/ClickHouseRawResult.cs
@@ -16,6 +16,12 @@ namespace ClickHouse.Client.ADO
 
         public Task<Stream> ReadAsStreamAsync() => response.Content.ReadAsStreamAsync();
 
+        public Task<byte[]> ReadAsByteArrayAsync() => response.Content.ReadAsByteArrayAsync();
+
+        public Task<string> ReadAsStringAsync() => response.Content.ReadAsStringAsync();
+
+        public Task CopyToAsync(Stream stream) => response.Content.CopyToAsync(stream);
+
         public void Dispose() => response?.Dispose();
     }
 }
