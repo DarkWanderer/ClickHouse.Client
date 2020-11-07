71813ab7007699603085b2175ad6b96a2a692267
DarkWanderer <elquentaro@gmail.com>
2020-11-07 22:21:47 +0300

    Dead code removal

--- ClickHouse.Client/Utility/StringExtensions.cs
+++ ClickHouse.Client/Utility/StringExtensions.cs
@@ -4,38 +4,6 @@ namespace ClickHouse.Client.Utility
 {
     public static class StringExtensions
     {
-        public static string TrimBrackets(this string input, char leftBracket, char rightBracket)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                return input;
-            }
-
-            if (input[0] != leftBracket || input[input.Length - 1] != rightBracket)
-            {
-                return input;
-            }
-
-            return input.Substring(1, input.Length - 2);
-        }
-
-        public static string TrimRoundBrackets(this string input) => TrimBrackets(input, '(', ')');
-
-        public static string TrimSquareBrackets(this string input) => TrimBrackets(input, '[', ']');
-
-        public static string TrimCurlyBraces(this string input) => TrimBrackets(input, '{', '}');
-
-        public static string ToHexString(this byte[] byteArray)
-        {
-            var hex = new StringBuilder(byteArray.Length * 2);
-
-            foreach (var b in byteArray)
-            {
-                hex.AppendFormat("{0:x2} ", b);
-            }
-            return hex.ToString();
-        }
-
         public static string Escape(this string str) => "'" + str.Replace("\\", "\\\\").Replace("\'", "\\\'") + "'";
     }
 }
