83a3c6b2d357b2d77ba623fe01e34f5ae3aca6fe
DarkWanderer <elquentaro@gmail.com>
2020-11-07 22:19:19 +0300

    Coverage: ClickHouseConnectionFactory

---
+++ ClickHouse.Client.Tests/ADO/ProviderFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using ClickHouse.Client.ADO;
+using ClickHouse.Client.ADO.Adapters;
+using ClickHouse.Client.ADO.Parameters;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests.ADO
+{
+    public class ProviderFactoryTests
+    {
+        [Test]
+        public void ShouldProduceCorrectTypes()
+        {
+            DbProviderFactory factory = new ClickHouseConnectionFactory();
+            Assert.IsInstanceOf<ClickHouseConnection>(factory.CreateConnection());
+            Assert.IsInstanceOf<ClickHouseCommand>(factory.CreateCommand());
+            Assert.IsInstanceOf<ClickHouseDataAdapter>(factory.CreateDataAdapter());
+            Assert.IsInstanceOf<ClickHouseConnectionStringBuilder>(factory.CreateConnectionStringBuilder());
+            Assert.IsInstanceOf<ClickHouseDbParameter>(factory.CreateParameter());
+
+            // TODO
+            // Assert.IsInstanceOf<ClickHouseConnectionStringBuilder>(factory.CreateCommandBuilder());
+        }
+    }
+}
