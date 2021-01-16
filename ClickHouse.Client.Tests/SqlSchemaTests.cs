5771fb9b5979fce2a759490667264ea7abbee4bc
DarkWanderer <elquentaro@gmail.com>
2021-01-16 15:30:33 +0300

    Extracted schema tests into separate file

---
+++ ClickHouse.Client.Tests/SqlSchemaTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+using ClickHouse.Client.Utility;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests
+{
+    public class SqlSchemaTests : AbstractConnectionTestFixture
+    {
+        [Test]
+        public async Task ShouldGetReaderColumnSchema()
+        {
+            using var reader = await connection.ExecuteReaderAsync("SELECT 1 as num, 'a' as str");
+            var schema = reader.GetColumnSchema();
+            Assert.AreEqual(2, schema.Count);
+            Assert.AreEqual("num", schema[0].ColumnName);
+            Assert.AreEqual("str", schema[1].ColumnName);
+        }
+
+        [Test]
+        public async Task ShouldGetReaderSchemaTable()
+        {
+            using var reader = await connection.ExecuteReaderAsync("SELECT 1 as num, 'a' as str");
+            var schema = reader.GetSchemaTable();
+            Assert.AreEqual(2, schema.Rows.Count);
+        }
+    }
+}
--- ClickHouse.Client.Tests/SqlSimpleSelectTests.cs
+++ ClickHouse.Client.Tests/SqlSimpleSelectTests.cs
@@ -180,24 +180,6 @@ namespace ClickHouse.Client.Tests
             }
         }
 
-        [Test]
-        public async Task ShouldGetReaderColumnSchema()
-        {
-            using var reader = await connection.ExecuteReaderAsync("SELECT 1 as num, 'a' as str");
-            var schema = reader.GetColumnSchema();
-            Assert.AreEqual(2, schema.Count);
-            Assert.AreEqual("num", schema[0].ColumnName);
-            Assert.AreEqual("str", schema[1].ColumnName);
-        }
-
-        [Test]
-        public async Task ShouldGetReaderSchemaTable()
-        {
-            using var reader = await connection.ExecuteReaderAsync("SELECT 1 as num, 'a' as str");
-            var schema = reader.GetSchemaTable();
-            Assert.AreEqual(2, schema.Rows.Count);
-        }
-
         public void Dispose() => connection?.Dispose();
     }
 }
