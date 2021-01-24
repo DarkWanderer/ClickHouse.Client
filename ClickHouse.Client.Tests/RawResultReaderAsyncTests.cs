ec2bc6367f6eb1ad127405682e27a890c58708b3
DarkWanderer <elquentaro@gmail.com>
2021-01-24 20:29:50 +0300

    Improving test coverage

--- ClickHouse.Client.Tests/RawResultReaderAsyncTests.cs
+++ ClickHouse.Client.Tests/RawResultReaderAsyncTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -8,14 +9,49 @@ namespace ClickHouse.Client.Tests
     public class RawResultReaderAsyncTests : AbstractConnectionTestFixture
     {
         [Test]
-        public async Task ShouldReadRawResult()
+        public async Task ShouldReadRawResultAsStream()
         {
             using var command = connection.CreateCommand();
             command.CommandText = "SELECT 1,2,3 FORMAT TSV";
             using var result = await command.ExecuteRawResultAsync(CancellationToken.None);
             using var stream = await result.ReadAsStreamAsync();
             using var reader = new StreamReader(stream);
-            Assert.AreEqual("1\t2\t3", reader.ReadToEnd().Trim());
+            Assert.AreEqual("1\t2\t3\n", reader.ReadToEnd());
+        }
+
+        [Test]
+        public async Task ShouldCopyRawResultToStream()
+        {
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT 1,2,3 FORMAT TSV";
+            using var result = await command.ExecuteRawResultAsync(CancellationToken.None);
+            using var stream = new MemoryStream();
+                await result.CopyToAsync(stream);
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var reader = new StreamReader(stream);
+            Assert.AreEqual("1\t2\t3\n", reader.ReadToEnd());
+        }
+
+        [Test]
+        public async Task ShouldReadRawResultAsArray()
+        {
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT 1,2,3 FORMAT TSV";
+            using var result = await command.ExecuteRawResultAsync(CancellationToken.None);
+            var array = await result.ReadAsByteArrayAsync();
+            Assert.AreEqual(Encoding.UTF8.GetBytes("1\t2\t3\n"), array);
+        }
+
+        [Test]
+        public async Task ShouldReadRawResultAsString()
+        {
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT 1,2,3 FORMAT TSV";
+            using var result = await command.ExecuteRawResultAsync(CancellationToken.None);
+            var @string = await result.ReadAsStringAsync();
+            Assert.AreEqual("1\t2\t3\n", @string);
         }
     }
 }
---
+++ ClickHouse.Client.Tests/Types/SyntaxParsingTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClickHouse.Client.Types.Grammar;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests.Syntax
+{
+    public class TypeGrammarParsingTests
+    {
+        [Test]
+        [TestCase("Int32")]
+        [TestCase("Nested(Int32, String, Tuple(UInt8, FixedString(3)))")]
+        [TestCase("Tuple(Int32, Tuple(UInt8, String, Nullable(Int32)))")]
+        public void ShouldRoundTripParsedType(string input)
+        {
+            var output = Parser.Parse(input);
+            Assert.AreEqual(input, output.ToString());
+        }
+    }
+}
