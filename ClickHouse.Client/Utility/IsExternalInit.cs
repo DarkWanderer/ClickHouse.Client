4195327b713b949c70c4064968f9d03bf5c4f150
DarkWanderer <elquentaro@gmail.com>
2021-11-24 14:00:50 +0300

    Added helper struct to allow "init" properties

---
+++ ClickHouse.Client/Utility/IsExternalInit.cs
@@ -0,0 +1,7 @@
+﻿// Helper definition to workaround VS2019 bug
+// CS0518 Predefined type 'System.Runtime.CompilerServices.IsExternalInit' is not defined or imported
+// See: https://stackoverflow.com/a/64749403/
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+}
