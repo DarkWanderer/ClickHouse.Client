fbf9cfaa20775113f9d04c8a6a598216505ee99b
Oleg V. Kozlyuk <elquentaro@gmail.com>
2022-10-15 02:15:27 +0300

    Code coverage diff in PRs (#210)

---
+++ ClickHouse.Client.Tests/Misc/ExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickHouse.Client.Utility;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests.Misc
+{
+    public class ExtensionsTests
+    {
+        [Test]
+        public void ShouldDeconstruct()
+        {
+            var list = new[] { 1, 2 };
+            var (first, _) = list;
+            Assert.AreEqual(1, first);
+        }
+    }
+}
