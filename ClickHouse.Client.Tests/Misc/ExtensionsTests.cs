1f12d4d46820aa0e5f3fcfea16229528f0b292e0
Oleg V. Kozlyuk <elquentaro@gmail.com>
2022-10-15 17:46:26 +0300

    Improved Deconstruct extension (#211)

--- ClickHouse.Client.Tests/Misc/ExtensionsTests.cs
+++ ClickHouse.Client.Tests/Misc/ExtensionsTests.cs
@@ -11,11 +11,29 @@ namespace ClickHouse.Client.Tests.Misc
     public class ExtensionsTests
     {
         [Test]
-        public void ShouldDeconstruct()
+        public void ShouldDeconstruct2()
         {
-            var list = new[] { 1, 2 };
-            var (first, _) = list;
-            Assert.AreEqual(1, first);
+            var (a1,a2) = new[] { 1, 2 };
+            Assert.AreEqual(1, a1);
+            Assert.AreEqual(2, a2);
+        }
+
+        [Test]
+        public void ShouldDeconstruct3()
+        {
+            var (b1, b2, b3) = new[] { 1, 2, 3 };
+            Assert.AreEqual(1, b1);
+            Assert.AreEqual(2, b2);
+            Assert.AreEqual(3, b3);
+        }
+
+        [Test]
+        public void ShouldThrowOnWrongCount()
+        {
+            Assert.Throws<ArgumentException>(() => { var (a1, a2) = new[] { 1 }; });
+            Assert.Throws<ArgumentException>(() => { var (a1, a2) = new[] { 1, 2, 3 }; });
+            Assert.Throws<ArgumentException>(() => { var (a1, a2, a3) = new[] { 1 }; });
+            Assert.Throws<ArgumentException>(() => { var (a1, a2, a3) = new[] { 1, 2, 3, 4 }; });
         }
     }
 }
