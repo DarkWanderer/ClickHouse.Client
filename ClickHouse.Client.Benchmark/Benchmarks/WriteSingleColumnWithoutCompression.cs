a4d44b5792905b8c563ae1c53d9fd1a29342de1b
DarkWanderer <elquentaro@gmail.com>
2022-10-20 12:32:28 +0200

    Benchmark tweaks

--- ClickHouse.Client.Benchmark/Benchmarks/WriteSingleColumnWithoutCompression.cs
+++ ClickHouse.Client.Benchmark/Benchmarks/WriteSingleColumnWithoutCompression.cs
@@ -16,9 +16,10 @@ namespace ClickHouse.Client.Benchmark.Benchmarks
 
         public override async Task<BenchmarkResult> Run()
         {
-            var count = Convert.ToInt32(Duration.TotalSeconds * 5000000.0);
+            var count = Convert.ToInt32(Duration.TotalSeconds * 25000000.0);
             Console.WriteLine("Preparing data");
-            var values = Enumerable.Range(0, count).Select(i => new object[] { (long)i }).ToList();
+            var row = new object[] { (long)0 };
+            var values = Enumerable.Range(0, count).Select(i => row);
             Console.WriteLine("Running benchmark");
 
             var targetDatabase = "benchmark";
@@ -29,8 +30,8 @@ namespace ClickHouse.Client.Benchmark.Benchmarks
             // Create database and table for benchmark
             using var targetConnection = GetConnection();
             await targetConnection.ExecuteStatementAsync($"CREATE DATABASE IF NOT EXISTS {targetDatabase}");
-            await targetConnection.ExecuteStatementAsync($"TRUNCATE TABLE IF EXISTS {targetTable}");
-            await targetConnection.ExecuteStatementAsync($"CREATE TABLE IF NOT EXISTS {targetTable} (col1 Int64) ENGINE Memory");
+            await targetConnection.ExecuteStatementAsync($"DROP TABLE IF EXISTS {targetTable}");
+            await targetConnection.ExecuteStatementAsync($"CREATE TABLE IF NOT EXISTS {targetTable} (col1 Int64) ENGINE Null");
 
             targetConnection.ChangeDatabase(targetDatabase);
 
@@ -46,11 +47,8 @@ namespace ClickHouse.Client.Benchmark.Benchmarks
             stopwatch.Stop();
 
             // Verify we've written expected number of rows
-            //Assert.AreEqual(count, bulkCopyInterface.RowsWritten);
+            Debug.Assert(count == bulkCopyInterface.RowsWritten);
             //Assert.AreEqual(count, Convert.ToInt32(await targetConnection.ExecuteScalarAsync($"SELECT COUNT(*) FROM {targetTable}")));
-
-            // Clear table after benchmark
-            await targetConnection.ExecuteStatementAsync($"TRUNCATE TABLE IF EXISTS {targetTable}");
 
             var rps = (long)count * 1000 / stopwatch.ElapsedMilliseconds;
             return new BenchmarkResult { Duration = stopwatch.Elapsed, RowsCount = Convert.ToUInt64(count), DataSize = Convert.ToUInt64(count) * sizeof(long) };
