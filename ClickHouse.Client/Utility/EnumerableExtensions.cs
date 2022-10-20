e2ef61647cbd872386f1c3fabdcc74ae34857e99
DarkWanderer <elquentaro@gmail.com>
2022-10-20 12:38:24 +0200

    Remove now-unused method

--- ClickHouse.Client/Utility/EnumerableExtensions.cs
+++ ClickHouse.Client/Utility/EnumerableExtensions.cs
@@ -6,36 +6,6 @@ namespace ClickHouse.Client.Utility
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<IList<T>> Batch<T>(this IEnumerable<T> source, int size)
-        {
-            T[] bucket = null;
-            var count = 0;
-
-            foreach (var item in source)
-            {
-                bucket ??= new T[size];
-
-                bucket[count++] = item;
-
-                if (count != size)
-                {
-                    continue;
-                }
-
-                yield return bucket;
-
-                bucket = null;
-                count = 0;
-            }
-
-            // Return the last bucket with all remaining elements
-            if (bucket != null && count > 0)
-            {
-                Array.Resize(ref bucket, count);
-                yield return bucket;
-            }
-        }
-
         public static void Deconstruct<T>(this IList<T> list, out T first, out T second)
         {
             if (list.Count != 2)
