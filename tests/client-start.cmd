0812ed206cd26e5bc5a12f04b86dbad2053b0e65
DarkWanderer <elquentaro@gmail.com>
2022-10-30 21:28:21 +0100

    Removed references to client timezone and personal workdir

--- tests/client-start.cmd
+++ tests/client-start.cmd
@@ -1,1 +1,1 @@
-docker run -it --rm --link clickhouse-server:clickhouse-server --entrypoint clickhouse-client clickhouse/clickhouse-server --host clickhouse-server --use_client_time_zone true
+docker run -it --rm --link clickhouse-server:clickhouse-server --entrypoint clickhouse-client clickhouse/clickhouse-server --host clickhouse-server
