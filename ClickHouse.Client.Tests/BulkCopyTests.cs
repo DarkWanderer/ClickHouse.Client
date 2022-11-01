18639b66ef1ee0aad4f38c80aa6cd4887fad2860
DarkWanderer <elquentaro@gmail.com>
2022-11-02 00:04:51 +0100

    Added explicit test for BulkCopy session exception

--- ClickHouse.Client.Tests/BulkCopyTests.cs
+++ ClickHouse.Client.Tests/BulkCopyTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -53,10 +54,49 @@ public class BulkCopyTests : AbstractConnectionTestFixture
         Assert.AreEqual(insertedValue, data, "Original and actually inserted values differ");
     }
 
-    [Test]
-    public async Task ShouldExecuteInsertWithLessColumns()
-    {
-        var targetTable = $"test.multiple_columns";
+        [Test]
+        [Explicit]
+        public async Task ShouldExecuteMultipleBulkInsertions()
+        {
+            var sw = new Stopwatch();
+            var duration = new TimeSpan(0, 5, 0);
+
+            var targetTable = "test." + SanitizeTableName($"bulk_load_test");
+
+            await connection.ExecuteStatementAsync($"DROP TABLE IF EXISTS {targetTable}");
+            await connection.ExecuteStatementAsync($"CREATE TABLE IF NOT EXISTS {targetTable} (value Int32) ENGINE Null");
+
+            var cb = TestUtilities.GetConnectionStringBuilder();
+            cb.UseSession = true;
+
+            var conn = new ClickHouseConnection(cb.ToString());
+            sw.Start();
+            var i = 0;
+            try
+            {
+                while (sw.Elapsed < duration)
+                {
+                    using var bulkCopy = new ClickHouseBulkCopy(conn)
+                    {
+                        DestinationTableName = targetTable,
+                        MaxDegreeOfParallelism = 8,
+                        BatchSize = 100
+                    };
+
+                    await bulkCopy.WriteToServerAsync(Enumerable.Repeat(new[] { (object)0 }, 1));
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Iteration: {i}", e);
+            }
+        }
+
+        [Test]
+        public async Task ShouldExecuteInsertWithLessColumns()
+        {
+            var targetTable = $"test.multiple_columns";
 
         await connection.ExecuteStatementAsync($"TRUNCATE TABLE IF EXISTS {targetTable}");
         await connection.ExecuteStatementAsync($"CREATE TABLE IF NOT EXISTS {targetTable} (value1 Nullable(UInt8), value2 Nullable(Float32), value3 Nullable(Int8)) ENGINE TinyLog");
