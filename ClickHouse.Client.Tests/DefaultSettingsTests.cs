b800806b4618c5d2f2fafc412ea433a90f27c310
Oleg V. Kozlyuk <elquentaro@gmail.com>
2023-01-28 16:59:39 +0300

    Adding test for default settings (#263)

---
+++ ClickHouse.Client.Tests/DefaultSettingsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickHouse.Client.ADO;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests
+{
+    public class DefaultSettingsTests
+    {
+        [Test]
+        public void DefaultSettingsShouldMatch()
+        {
+            var builder = new ClickHouseConnectionStringBuilder();
+            Assert.AreEqual(true, builder.UseCustomDecimals);
+            Assert.AreEqual(true, builder.Compression);
+            Assert.AreEqual(true, builder.UseServerTimezone);
+            Assert.AreEqual(false, builder.UseSession);
+        }
+    }
+}
