d8841eef037387a0e6f669a0e0e13aeb97155306
DarkWanderer <elquentaro@gmail.com>
2023-03-07 15:25:15 +0100

    Default to port 8443 if Protocol=https #274

---
+++ ClickHouse.Client.Tests/ADO/ConnectionStringBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickHouse.Client.ADO;
+using NUnit.Framework;
+
+namespace ClickHouse.Client.Tests.ADO;
+
+public class ConnectionStringBuilderTests
+{
+    [Test]
+    public void ShouldHaveReasonableDefaults()
+    {
+        Assert.AreEqual(8123, new ClickHouseConnectionStringBuilder().Port);
+        Assert.AreEqual(8443, new ClickHouseConnectionStringBuilder("Protocol=https").Port);
+        Assert.AreEqual("default", new ClickHouseConnectionStringBuilder().Database);
+        Assert.AreEqual("default", new ClickHouseConnectionStringBuilder().Username);
+    }
+}
