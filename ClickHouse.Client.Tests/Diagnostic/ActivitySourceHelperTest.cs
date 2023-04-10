bda5b2843620d5917e89f0bbcef6c1d8fac68160
lin.qingliang <chad122@163.com>
2023-04-10 10:49:15 +0800

    add unit test of ActivitySourceHelper

---
+++ ClickHouse.Client.Tests/Diagnostic/ActivitySourceHelperTest.cs
@@ -0,0 +1,89 @@
+﻿using ClickHouse.Client.Diagnostic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickHouse.Client.Tests.Diagnostic
+{
+    public class ActivitySourceHelperTest
+    {
+        private const string testConnectionString = "compression=True;timeout=30;database=test;username=test;password=123456;protocol=http;host=192.168.1.1;port=4567;set_session_timeout=1";
+        private const string testSql = "select 1;";
+        private Activity? InitActivity(bool addListener)
+        {
+            if (addListener)
+            {
+                var options = new ActivityCreationOptions<ActivityContext>();
+                ActivitySource.AddActivityListener(new ActivityListener
+                {
+                    ShouldListenTo = source => { return true; },
+                    SampleUsingParentId = (ref ActivityCreationOptions<string> activityOptions) => ActivitySamplingResult.AllData,
+                    Sample = (ref ActivityCreationOptions<ActivityContext> activityOptions) => ActivitySamplingResult.AllData
+                });
+            }
+            return ActivitySourceHelper.StartActivity("ActivitySourceHelperTest");
+        }
+
+        [TestCase(true, null, null)]
+        [TestCase(true, testConnectionString, null)]
+        [TestCase(true, null, testSql)]
+        [TestCase(false, null, null)]
+        [TestCase(false, testConnectionString, null)]
+        [TestCase(false, null, testSql)]
+        public void ShouldNotThrowExceptionWhenSetConnectionTags(bool addListener, string? connectionString, string? sql)
+        {
+            var activity = InitActivity(addListener);
+            activity.SetConnectionTags(connectionString, sql);
+            activity.SetSuccess();
+
+            if (addListener)
+            {
+                Assert.IsNotNull(activity);
+
+                Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_ThreadId));
+                Assert.AreEqual(ActivitySourceHelper.Value_DbSystem, activity.GetTagItem(ActivitySourceHelper.Tag_DbSystem));
+                Assert.AreEqual(ActivitySourceHelper.Value_StatusOK, activity.GetTagItem(ActivitySourceHelper.Tag_StatusCode));
+                if (connectionString != null)
+                {
+                    Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_DbConnectionString));
+                    Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_DbName));
+                    Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_User));
+                    Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_Service));
+                }
+                if (sql != null)
+                {
+                    Assert.AreEqual(sql, activity.GetTagItem(ActivitySourceHelper.Tag_DbStatement));
+                }
+            }
+        }
+
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public void ShouldNotThrowExceptionWhenSetException(bool addListener, bool exceptionIsNull)
+        {
+            var activity = InitActivity(addListener);
+            Exception? exception = exceptionIsNull ? null : new Exception("test");
+            activity.SetException(exception);
+
+            if (addListener)
+            {
+                Assert.IsNotNull(activity);
+
+                Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_ThreadId));
+                Assert.AreEqual(ActivitySourceHelper.Value_DbSystem, activity.GetTagItem(ActivitySourceHelper.Tag_DbSystem));
+                Assert.AreEqual(ActivitySourceHelper.Value_StatusERROR, activity.GetTagItem(ActivitySourceHelper.Tag_StatusCode));
+                if (!exceptionIsNull)
+                {
+                    Assert.IsNotNull(activity.GetTagItem(ActivitySourceHelper.Tag_StatusDescription));
+                    Assert.True(activity.Events.Any(x => x.Name == "exception"));
+                }
+            }
+        }
+    }
+}
--- ClickHouse.Client/Diagnostic/ActivitySourceHelper.cs
+++ ClickHouse.Client/Diagnostic/ActivitySourceHelper.cs
@@ -16,51 +16,70 @@ namespace ClickHouse.Client.Diagnostic
         internal const string Tag_DbStatement = "db.statement";
         internal const string Tag_DbSystem = "db.system";
         internal const string Tag_StatusCode = "otel.status_code";
+        internal const string Tag_StatusDescription = "otel.status_description";
         internal const string Tag_User = "db.user";
         internal const string Tag_Service = "peer.service";
         internal const string Tag_ThreadId = "thread.id";
 
         internal const string Value_DbSystem = "clickhouse";
+        internal const string Value_StatusOK = "OK";
+        internal const string Value_StatusERROR = "ERROR";
         internal const int StatementMaxLen = 300;
 
         internal static Activity? StartActivity(string name)
         {
             var activity = ActivitySource.StartActivity(name, ActivityKind.Client, default(ActivityContext));
-            if (activity is { IsAllDataRequested: true })
+            if (activity == null || !activity.IsAllDataRequested)
             {
-                activity.SetTag(Tag_ThreadId, Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture));
-                activity.SetTag(Tag_DbSystem, Value_DbSystem);
+                return activity;
             }
+            activity.SetTag(Tag_ThreadId, Environment.CurrentManagedThreadId.ToString(CultureInfo.InvariantCulture));
+            activity.SetTag(Tag_DbSystem, Value_DbSystem);
             return activity;
         }
 
-        internal static void SetConnectionTags(this Activity? activity, string connectionString, string? sql)
+        internal static void SetConnectionTags(this Activity? activity, string? connectionString, string? sql)
         {
-            var builder = new ClickHouseConnectionStringBuilder() { ConnectionString = connectionString };
-            activity?.SetTag(Tag_DbConnectionString, connectionString);
-            activity?.SetTag(Tag_DbName, builder.Database);
-            activity?.SetTag(Tag_User, builder.Username);
-            activity?.SetTag(Tag_Service, new UriBuilder(builder.Protocol, builder.Host, builder.Port).Uri.ToString());
+            if (activity == null || !activity.IsAllDataRequested)
+            {
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                var builder = new ClickHouseConnectionStringBuilder() { ConnectionString = connectionString };
+                activity?.SetTag(Tag_DbConnectionString, connectionString);
+                activity?.SetTag(Tag_DbName, builder.Database);
+                activity?.SetTag(Tag_User, builder.Username);
+                activity?.SetTag(Tag_Service, new UriBuilder(builder.Protocol, builder.Host, builder.Port).Uri.ToString());
+            }
             activity?.SetTag(Tag_DbStatement, sql is not null && sql.Length > StatementMaxLen ? sql.Substring(0, StatementMaxLen) : sql);
         }
 
         internal static void SetSuccess(this Activity? activity)
         {
+            if (activity == null || !activity.IsAllDataRequested)
+            {
+                return;
+            }
 #if NET6_0_OR_GREATER
             activity?.SetStatus(ActivityStatusCode.Ok);
 #endif
-            activity?.SetTag(Tag_StatusCode, "OK");
+            activity?.SetTag(Tag_StatusCode, Value_StatusOK);
             activity?.Stop();
         }
 
         internal static void SetException(this Activity? activity, Exception? exception)
         {
-            var description = exception.Message;
+            if (activity == null || !activity.IsAllDataRequested)
+            {
+                return;
+            }
+            var description = exception?.Message;
 #if NET6_0_OR_GREATER
             activity?.SetStatus(ActivityStatusCode.Error, description);
 #endif
-            activity?.SetTag(Tag_StatusCode, "ERROR");
-            activity?.SetTag("otel.status_description", description);
+            activity?.SetTag(Tag_StatusCode, Value_StatusERROR);
+            activity?.SetTag(Tag_StatusDescription, description);
             activity?.AddEvent(new ActivityEvent("exception", tags: new ActivityTagsCollection
             {
                 { "exception.type", exception?.GetType().FullName },
