ac4a1c796168794d8573d6f0fe739ce1565d445a
Oleg V. Kozlyuk <elquentaro@gmail.com>
2023-08-20 13:21:27 +0200

    Switch to NSubstitute due to privacy concerns (#360)

--- ClickHouse.Client.Tests/Formats/PeekableStreamWrapperTests.cs
+++ ClickHouse.Client.Tests/Formats/PeekableStreamWrapperTests.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
 using ClickHouse.Client.Formats;
-using Moq;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ClickHouse.Client.Tests.Formats;
@@ -10,31 +10,27 @@ public class PeekableStreamWrapperTests
     [Test]
     public void PeekableStreamWrapperTestSuite()
     {
-        var streamMock = new Mock<Stream>();
-        using var wrapper = new PeekableStreamWrapper(streamMock.Object);
+        var streamMock = Substitute.For<Stream>();
+        using var wrapper = new PeekableStreamWrapper(streamMock);
 
-        streamMock.Setup(x => x.CanRead).Returns(true);
-        Assert.AreEqual(true, wrapper.CanRead);
 
-        streamMock.Setup(x => x.CanWrite).Returns(false);
-        Assert.AreEqual(false, wrapper.CanWrite);
+        streamMock.CanSeek.Returns(false);
+        streamMock.CanRead.Returns(true);
+        streamMock.CanWrite.Returns(false);
+        streamMock.Length.Returns(999999);
 
-        streamMock.Setup(x => x.CanSeek).Returns(false);
         Assert.AreEqual(false, wrapper.CanSeek);
-
-        streamMock.Setup(x => x.Length).Returns(999999);
+        Assert.AreEqual(true, wrapper.CanRead);
+        Assert.AreEqual(false, wrapper.CanWrite);
         Assert.AreEqual(999999, wrapper.Length);
 
-        streamMock.Setup(x => x.Flush());
         wrapper.Flush();
-        streamMock.Verify(m => m.Flush(), Times.Once());
+        streamMock.Received().Flush();
 
-        streamMock.Setup(x => x.SetLength(123456));
         wrapper.SetLength(123456);
-        streamMock.Verify(m => m.SetLength(123456), Times.Once());
+        streamMock.Received().SetLength(123456);
 
-        streamMock.Setup(x => x.Seek(1, SeekOrigin.Begin));
         wrapper.Seek(1, SeekOrigin.Begin);
-        streamMock.Verify(m => m.Seek(1, SeekOrigin.Begin), Times.Once());
+        streamMock.Received().Seek(1, SeekOrigin.Begin);
     }
 }
