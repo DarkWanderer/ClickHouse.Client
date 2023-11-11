05c3030b79def082d04b2cd83e9db282bbcedd7f
DarkWanderer <elquentaro@gmail.com>
2023-11-11 20:41:07 +0100

    Add coverage diff

--- .github/coverage-status.py
+++ .github/coverage-status.py
@@ -1,5 +1,6 @@
 from pycobertura import Cobertura
 import argparse
+import json
 import os
 import requests
 
@@ -11,9 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--coverage-file", required=True,
-                        help="Path of pycobertura coverage XML file")
-    parser.add_argument("--coverage-diff-file",
-                        help="Path of pycobertura diff .json file")
+                        help="Path of cobertura XML file or pycobertura .diff.json")
     parser.add_argument("--repository", required=True,
                         help="owner/name of repository")
     parser.add_argument("--sha", required=True, help="SHA hash of commit")
@@ -39,7 +38,17 @@ def post_status(repository: str, sha: str, state: str, context: str, description
 if __name__ == "__main__":
     args = parse_args()
 
-    cobertura = Cobertura(args.coverage_file)
-
-    post_status(args.repository, args.sha, "success", "Coverage / Total",
-                f"line: {cobertura.line_rate() * 100:.2f}% branch: {cobertura.branch_rate() * 100:.2f}%")
+    name, ext = os.path.splitext(args.coverage_file)
+    if ext == '.xml':
+        cobertura = Cobertura(args.coverage_file)
+        post_status(args.repository, args.sha, "success", "Coverage / Total",
+                    f"line: {cobertura.line_rate() * 100:.2f}% branch: {cobertura.branch_rate() * 100:.2f}%")
+    elif ext == '.json':
+        with open(args.coverage_file) as f:
+            data = json.load(f)
+            chnge = data['total']["Cover"]
+            stmts = data['total']["Stmts"]
+            post_status(args.repository, args.sha, "success", "Coverage / Diff",
+                        f"change: {chnge}, statements: {stmts}")
+    else:
+        raise Exception(f'Unknown file type {ext} for {args.coverage_file}')
