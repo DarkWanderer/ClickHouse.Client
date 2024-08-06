679d7db9af29226f370c8150868555f4c355574f
Pavel Kravtsov <p.kravtsov@tools4brokers.com>
2024-08-06 23:19:42 +0700

    Add additional metrics to QueryStats

--- ClickHouse.Client/ADO/QueryStats.cs
+++ ClickHouse.Client/ADO/QueryStats.cs
@@ -1,5 +1,21 @@
 ﻿namespace ClickHouse.Client.ADO;
 
 #pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-public record class QueryStats(long ReadRows, long ReadBytes, long WrittenRows, long WrittenBytes, long TotalRowsToRead);
+
+public record QueryStats(
+    long ReadRows,
+    long ReadBytes,
+    long WrittenRows,
+    long WrittenBytes,
+    long TotalRowsToRead,
+    long? ResultRows,
+    long? ResultBytes,
+    long ElapsedNs)
+{
+    internal QueryStats WithResponseBufferingDisabled()
+    {
+        return this with { ResultRows = null, ResultBytes = null };
+    }
+}
+
 #pragma warning restore SA1313 // Parameter names should begin with lower-case letter
