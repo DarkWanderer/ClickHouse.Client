57ac08b53a65a06f9d4b977a51db7acef18ae6d5
SMAH1 <integral22003@yahoo.com>
2024-11-21 11:10:43 +0330

    Test for default constructor for BulkCopy (#550)

---
+++ ClickHouse.Client.Tests/ORM/LinqToDbTests.cs
@@ -0,0 +1,24 @@
+﻿using ClickHouse.Client.Copy;
+using NUnit.Framework;
+using System;
+
+namespace ClickHouse.Client.Tests.ORM;
+
+public class LinqToDbTests : AbstractConnectionTestFixture
+{
+    [Test]
+    public void ShouldBulkCopyHasConstructorWithOneParameter()
+    {
+        ClickHouseBulkCopy cpy = null;
+        try
+        {
+            cpy = (ClickHouseBulkCopy)Activator.CreateInstance(typeof(ClickHouseBulkCopy), new object[] { connection });
+        }
+        catch
+        {
+            cpy = null;
+        }
+
+        Assert.IsNotNull(cpy);
+    }
+}
